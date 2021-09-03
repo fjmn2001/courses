85b3e2829afca889a19283c0e291f962c0b49499
Francisco Marcano <fjmn2001@gmail.com>
2021-09-03 13:18:42 -0400

    feat: added interface basic example. Ref sn

---
+++ apps/bases/interfaces/basic.ts
@@ -0,0 +1,24 @@
+(() => {
+    interface Hero {
+        name: string,
+        age: number,
+        powers: string[],
+        getName?: () => string
+    }
+
+    let flash: Hero = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['fast', 'time']
+    }
+
+    let superman: Hero = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: ['fast', 'time'],
+        getName: () => superman.name
+    }
+
+
+    console.log(flash, superman)
+})()
