55e9aa81177353be9b25bc0a2441e1021401507c
Francisco Marcano <fjmn2001@gmail.com>
2021-09-07 13:51:31 -0400

    feat: added namespace example. Ref sn

---
+++ apps/bases/namespaces/validations.ts
@@ -0,0 +1,16 @@
+namespace Validations {
+
+    export const validateText = (text: string): boolean => {
+        return text.length > 3
+    }
+
+    export const validateDate = (date: Date): boolean => {
+        return !isNaN(date.valueOf())
+    }
+}
+
+
+console.log(
+    Validations.validateText('Fer'),
+    Validations.validateDate(new Date())
+)
