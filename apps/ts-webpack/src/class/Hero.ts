5bca03b8cc47f3e1ac44919443b2308a694bd67b
Francisco Marcano <fjmn2001@gmail.com>
2021-09-13 13:28:29 -0400

    feat: added import export example v2. Ref sn

--- apps/ts-webpack/src/class/Hero.ts
+++ apps/ts-webpack/src/class/Hero.ts
@@ -1,8 +1,14 @@
+import powers from "../data/powers";
+
 export class Hero {
     constructor(
         public name: string,
         public powerId: number,
         public age: number
     ) {
+    }
+
+    get power() {
+        return powers.find(power => power.id === this.powerId)?.desc || 'not found'
     }
 }
--- apps/ts-webpack/src/index.ts
+++ apps/ts-webpack/src/index.ts
@@ -1,11 +1,12 @@
 import {Hero} from "./class/Hero";
+import powers from "./data/powers";
 
 
 const ironman: Hero = new Hero('Ironman', 1, 55)
 
 
-console.log(ironman);
-
+console.log(ironman.power);
+console.log(powers)
 
 
 
