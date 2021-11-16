6a2eb97bf900dbd7667c4b043df659913aedc020
🚀 Francisco Marcano <fjmn2001@gmail.com>
2021-11-16 13:00:54 -0400

    feat: adding first hello world. Ref sn

---
+++ apps/03-couter-app/src/FirstApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FirstApp = () => {
+    return (
+        <>
+            <h1>Hi there!</h1>
+            <p>This is a paragraph</p>
+        </>
+    )
+}
+
+export default FirstApp;
