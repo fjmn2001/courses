919c58ce2b7d4b9c9d4eece9d10626aa6a07b73c
🚀 Francisco Marcano <fjmn2001@gmail.com>
2021-11-18 18:16:54 -0400

    feat: adding testing v4. Ref sn

--- apps/03-couter-app/src/tests/FirstApp.test.js
+++ apps/03-couter-app/src/tests/FirstApp.test.js
@@ -1,11 +1,18 @@
-import {render} from "@testing-library/react";
+import {shallow} from "enzyme";
 import FirstApp from "../FirstApp";
 
 describe('Testing FirstApp.js', function () {
-    test('should show a greeting: "Hi Francisco!"', () => {
-        const greeting = `Hi Francisco!`
-        const {getByText} = render(<FirstApp greeting={greeting}/>)
+    // test('should show a greeting: "Hi Francisco!"', () => {
+    //     const greeting = `Hi Francisco!`
+    //     const {getByText} = render(<FirstApp greeting={greeting}/>)
+    //
+    //     expect(getByText(greeting)).toBeInTheDocument()
+    // })
 
-        expect(getByText(greeting)).toBeInTheDocument()
+    test('should show <FirstApp/> right', () => {
+        const greeting = `Hi there!`
+        const wrapper = shallow(<FirstApp greeting={greeting}/>)
+
+        expect(wrapper).toMatchSnapshot()
     })
 });
