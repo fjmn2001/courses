0822ebde8308a319c6ced068ce5dda5d7752b1cf
🚀 Francisco Marcano <fjmn2001@gmail.com>
2021-11-23 15:28:03 -0400

    feat: adding animation.css. Ref sn

--- apps/04-git-expert-app/src/components/GifGrid.tsx
+++ apps/04-git-expert-app/src/components/GifGrid.tsx
@@ -12,9 +12,9 @@ const GifGrid = ({category}: GifGridProps) => {
 
     return (
         <>
-            <h3>{category}</h3>
+            <h3 className={'animate__animated animate__fadeIn'}>{category}</h3>
 
-            {loading && <p>Loading</p>}
+            {loading && <p className={'animate__animated animate__flash'}>Loading</p>}
 
             <div className={'card-grid'}>
                 {images.map((gif: CustomGif) => {
--- apps/04-git-expert-app/src/components/GifGridItem.tsx
+++ apps/04-git-expert-app/src/components/GifGridItem.tsx
@@ -2,7 +2,7 @@ import {CustomGif} from "../interfaces";
 
 const GifGridItem = ({title, url}: CustomGif) => {
     return (
-        <div className={'card'}>
+        <div className={'card animate__animated animate__fadeIn'}>
             <img src={url} alt={title}/>
             <p>{title}</p>
         </div>
