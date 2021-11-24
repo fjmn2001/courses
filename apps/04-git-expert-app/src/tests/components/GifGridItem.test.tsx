e9763afe95fad091a254d66866fe20dd57401cc1
🚀 Francisco Marcano <fjmn2001@gmail.com>
2021-11-23 23:11:01 -0400

    feat: adding testing v2. Ref sn

--- apps/04-git-expert-app/src/tests/components/GifGridItem.test.tsx
+++ apps/04-git-expert-app/src/tests/components/GifGridItem.test.tsx
@@ -8,4 +8,32 @@ describe('Testing GifGridItem', () => {
 
         expect(wrapper).toMatchSnapshot()
     })
+
+    test('should have a paragraph with the title', () => {
+        const gif = {id: 'custom-id', title: 'Custom title', url: 'https://custom-url'}
+        const wrapper = shallow(<GifGridItem {...gif}/>)
+
+        const title = wrapper.find('p')
+
+        expect(title.text().trim()).toBe(gif.title)
+    })
+
+    test('should have image and alt like the props', () => {
+        const gif = {id: 'custom-id', title: 'Custom title', url: 'https://custom-url'}
+        const wrapper = shallow(<GifGridItem {...gif}/>)
+
+        const img = wrapper.find('img')
+
+        expect(img.prop('src')).toBe(gif.url)
+        expect(img.prop('alt')).toBe(gif.title)
+    })
+
+    test('should have a div with fadeIn className', () => {
+        const gif = {id: 'custom-id', title: 'Custom title', url: 'https://custom-url'}
+        const wrapper = shallow(<GifGridItem {...gif}/>)
+
+        const div = wrapper.find('div')
+
+        expect(div.hasClass('animate__fadeIn')).toBe(true)
+    })
 })
