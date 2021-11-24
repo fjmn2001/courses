a2c662032755b4dd817a7acb5500b034dbabd314
🚀 Francisco Marcano <fjmn2001@gmail.com>
2021-11-24 12:39:13 -0400

    feat: testing v1. Ref sn

--- apps/04-git-expert-app/src/tests/components/AddCategory.test.tsx
+++ apps/04-git-expert-app/src/tests/components/AddCategory.test.tsx
@@ -2,6 +2,11 @@ import {shallow} from "enzyme";
 import AddCategory from "../../components/AddCategory";
 
 describe('Testing <AddCategory/>', function () {
+
+    beforeEach(() => {
+        jest.clearAllMocks()
+    })
+
     test('should show <AddCategory/> right', () => {
         const setCategories = () => true;
         const wrapper = shallow(<AddCategory setCategories={setCategories}/>)
@@ -18,5 +23,36 @@ describe('Testing <AddCategory/>', function () {
         input.simulate('change', {target: {value}})
 
         expect(wrapper.find('input').prop('value')).toBe(value)
+    })
+
+    test('should not send the form when input is empty', () => {
+        const setCategories = jest.fn();
+        const wrapper = shallow(<AddCategory setCategories={setCategories}/>)
+        const form = wrapper.find('form')
+
+        form.simulate('submit', {
+            preventDefault() {
+            }
+        })
+
+        expect(setCategories).not.toHaveBeenCalled()
+    })
+
+    test('should send the form and clear the input text', () => {
+        const setCategories = jest.fn();
+        const wrapper = shallow(<AddCategory setCategories={setCategories}/>)
+
+        const input = wrapper.find('input')
+        const value = 'Hi there!';
+        input.simulate('change', {target: {value}})
+
+        const form = wrapper.find('form')
+        form.simulate('submit', {
+            preventDefault() {
+            }
+        })
+
+        expect(setCategories).toHaveBeenCalled()
+        expect(wrapper.find('input').prop('value')).toBe('')
     })
 });
